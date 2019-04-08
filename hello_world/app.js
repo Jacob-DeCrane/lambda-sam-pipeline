7efb0e76249f82b60f41d6a6b5c29e71928106a5
Jacob DeCrane <Jacob.Decrane@ge.com>
2019-04-08 10:00:26 -0400

    change'

--- hello_world/app.js
+++ hello_world/app.js
@@ -43,7 +43,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello, Jacob.',
+                message: 'hello, Jacob DeCrane.',
                 location: ret.data.trim()
             })
         }
